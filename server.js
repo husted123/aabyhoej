db388d32d3d82746bbd3b0b2886d247ebf991e38
husted123 <anders.husted@gmail.com>
2021-04-19 12:38:54 +0200

    port

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ if (process.env.NODE_ENV !== 'production') {
     next()
   }
   
- // app.listen(3000)
+var PORT = process.env.PORT || 3000;
 
 
   app.use("/stylesheets",express.static(__dirname + "/stylesheets"));
